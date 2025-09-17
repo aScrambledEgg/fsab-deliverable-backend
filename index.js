c6fe6be7286bb90917ddfdde4f8b6a7eee3b2a01
aScrambledEgg <121683586+aScrambledEgg@users.noreply.github.com>
2025-09-17 14:45:30 -0400

    Initialize firebase-template

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+dotenv.config()
+
+import { db } from "./util/FirebaseInit.js";
+import { collection } from "firebase/firestore"
+
+const app = express()
+const port = 8080;
+
+app.use(express.json())
+app.use(
+	cors({
+		origin: "http://localhost:3000"
+	})
+)
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Create a route at http://localhost:8080/testRoute. You can try it with your browser!
+app.get("/", async (req, res) => {
+	res.send("Hello World!");
+});
+
+// TODO: create an endpoint that fetches all students from Firebase
+app.get("/students", async (req, res) => {
+	res.send("TODO: Fetch all students")
+})
+
+function start() {
+	app.listen(port, () => {
+		console.log(`Started listening on http://localhost:${port}`)
+	})
+}
+
+start()
